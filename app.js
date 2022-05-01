8260ddbceb332b94b02be4ab1ffcbbaebe92bf83
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-05-01 21:46:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+let file = 'new.jpg'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAG4K9rR4HyLzPBcvh9rPfPdm8kmA6PsOE",
+  authDomain: "project9-4f058.firebaseapp.com",
+  projectId: "project9-4f058",
+  storageBucket: "project9-4f058.appspot.com",
+  messagingSenderId: "908366468736",
+  appId: "1:908366468736:web:1a5624c9a97d8b78cdf865",
+  measurementId: "G-4YT9120WMB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const storage = getStorage();
+const storageRef = ref(storage, 'new.jpg');
+
+const btn = document.getElementById("butt");
+
+      btn.addEventListener("click", function () {
+
+        uploadBytes(storageRef, file).then((snapshot) => {
+          console.log('Uploaded a blob or file!');
+        });
+ });
