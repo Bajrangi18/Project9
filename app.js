a399e1d4540028a197948df9949af53ddd760702
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-05-02 22:35:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,20 @@ let streamStarted = false;
 const constraints = {
   video: {
     width: {
-      min: 720,
-      ideal: 720,
-      max: 1080,
-    },
-    height: {
-      min: 720,
+       min: 720,
       ideal: 1080,
       max: 1440
+//       min: 720,
+//       ideal: 720,
+//       max: 1080,
+    },
+    height: {
+       min: 720,
+      ideal: 720,
+      max: 1080
+//       min: 720,
+//       ideal: 1080,
+//       max: 1440
     },
   }
 };
