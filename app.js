a6a17310b6ba192b8e771b5915f464556a5fd556
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-05-03 02:22:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,8 +69,15 @@ const database = getDatabase(app);
                  console.log(detectVal);
                  // console.log(detectVal);
           }
-
-          if(detectVal>=0.967){
+          if(detectVal<0.7){
+            document.getElementById("verify_emo").style.visibility = "hidden";
+            document.getElementById("verify_text").style.top = "65vh";
+            document.getElementById("verify_text").innerHTML = "Smile for the Camera!";
+          }
+          if(detectVal>=0.7){
+            document.getElementById("verify_emo").style.visibility="visible";
+            document.getElementById("verify_text").style.top = "70vh";
+            document.getElementById("verify_text").innerHTML = "Face Detected!";
             myScreen();
           }
 
